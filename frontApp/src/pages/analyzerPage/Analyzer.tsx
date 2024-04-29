d03a335a742053b0bbc15c106afd220f79dd1b6a
romainniamor <romainnavoret@gmail.com>
2024-04-29 17:43:49 +0200

    ♻️ Create DialogueBox for analyzer

--- frontApp/src/pages/analyzerPage/Analyzer.tsx
+++ frontApp/src/pages/analyzerPage/Analyzer.tsx
@@ -1,18 +1,14 @@
 import { MdFileUpload } from "react-icons/md";
 import Button from "../../components/reusableUi/Button";
 import { GrPowerReset } from "react-icons/gr";
-import { useState, useRef, useEffect } from "react";
-import { getData, sendFile } from "../../api/analyzerApi";
+import { useState, useRef } from "react";
+import { sendFile } from "../../api/analyzerApi";
+import DialogueBox from "./DialogueBox";
 
 export default function Analyzer() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [fileInfo, setFileInfo] = useState<File | null>(null as File | null);
   const [isUploaded, setIsUploaded] = useState<boolean>(false);
-
-  //will be removed
-  useEffect(() => {
-    getData();
-  }, []);
 
   const handleFileUpload = async (e) => {
     const file = e.target.files[0]; // Récupération du fichier sélectionné
@@ -23,7 +19,7 @@ export default function Analyzer() {
     }
     const formData = new FormData();
     formData.append("file", file); // Ajout du fichier à formData
-    console.log("file", file, formData);
+
     sendFile(formData);
   };
 
@@ -68,19 +64,11 @@ export default function Analyzer() {
         </div>
         <div className="flex-1 bg-gradient-to-b from-transparent to-red-200">
           <div className="w-full h-[600px] overflow-y-scroll flex flex-col gap-4 p-4">
-            <div className="flex justify-end">
-              <p className="px-5 py-3 rounded-xl shadow-sm max-w-md bg-white">
-                Hi there, i'm your personal assistant and a very fast reader,
-                upload a pdf doc and let see what i can do for you!
-              </p>
-            </div>
-
-            <div className="flex justify-start">
-              <p className="px-5 py-3 rounded-xl shadow-sm max-w-md bg-white">
-                Hi there, i'm your personal assistant and a very fast reader,
-                upload a pdf doc and let see what i can do for you!
-              </p>
-            </div>
+            <DialogueBox
+              content="Hi there, i'm your personal assistant and a very fast reader,
+                upload a pdf doc and let see what i can do for you!"
+              position={"justify-end"}
+            />
           </div>
           <div className="flex justify-center items-center  py-4">
             <form
