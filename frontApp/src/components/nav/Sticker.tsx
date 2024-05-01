7c959a9aa6e49914762a293eddefe3834715e106
romainniamor <romainnavoret@gmail.com>
2024-05-02 00:26:58 +0200

    🚧 Create navBar Sticker component

---
+++ frontApp/src/components/nav/Sticker.tsx
@@ -0,0 +1,7 @@
+export default function Sticker() {
+  return (
+    <div className="absolute top-[-3px] right-[-20px] bg-blue-500  px-1 font-bold rounded-lg text-white uppercase text-xxs flex justify-center items-center animate-bounce">
+      new
+    </div>
+  );
+}
